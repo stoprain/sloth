3686537af84b85f08ed92e0a918c1497613bd7bd
stoprain <mystoprain@gmail.com>
2022-11-18 17:51:17 +0800

    init repo

---
+++ sloth/slothApp.swift
@@ -0,0 +1,17 @@
+//
+//  slothApp.swift
+//  sloth
+//
+//  Created by Rain Qian on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct slothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

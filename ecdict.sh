1005034eba040af0146d5f44a5c8b1beb318f6e0
stoprain <mystoprain@gmail.com>
2022-11-25 10:27:45 +0800

    progress

---
+++ ecdict.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ ! -d "ECDICT" ] 
+then
+    git clone https://github.com/skywind3000/ECDICT
+fi
+
+python3 -c "from ECDICT import stardict; stardict.convert_dict('ecdict.csv', 'ecdict.db')"
